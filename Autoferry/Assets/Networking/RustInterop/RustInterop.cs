ced2474326015533b520196c54c70701a741b79e
Skarsh <skarshaug.thomas@gmail.com>
2020-07-15 18:38:40 +0200

    Experimenting with rust interop for streaming sensor data. Need to compile project as unsafe because of passing pointers to rust library.

---
+++ Autoferry/Assets/Networking/RustInterop/RustInterop.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System;
+
+public static class RustInterop {
+
+    [StructLayout(LayoutKind.Sequential)]
+    struct ArrayRef
+    {
+        public IntPtr Bytes;
+        public uint Length;
+    }
+
+    [DllImport("unity_rust")]
+    private static extern int get_random_int();
+
+    [DllImport("unity_rust")]
+    private static extern int multiply_by_two(int num);
+
+    [DllImport("unity_rust")]
+    private static extern int add_x_to_each_element(ArrayRef array, uint count);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int GetRandomInt()
+    {
+        return get_random_int();
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int MultiplyByTwo(int num)
+    {
+        return multiply_by_two(num);
+    }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public unsafe static int AddXToEachElement(byte[] array, uint length)
+    {
+        //return add_x_to_each_element(x, arr_ptr, length);
+        ArrayRef arrayRef = new ArrayRef();
+
+        IntPtr ptr;
+
+        fixed (byte* p = array)
+        {
+            ptr = (IntPtr)p;
+        }
+
+        arrayRef.Bytes = ptr;
+        arrayRef.Length = length;
+
+        return add_x_to_each_element(arrayRef, arrayRef.Length);
+        
+        
+
+    }
+}
---
+++ Autoferry/Assets/Networking/RustInterop/RustInteropTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RustInteropTest: MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        byte[] arr = { 104, 104, 104, 104, 104, 104 };
+
+        int randomNum = RustInterop.GetRandomInt();
+        Debug.Log("Random number: " + randomNum);
+        Debug.Log("Multiply by two: " + RustInterop.MultiplyByTwo(randomNum));
+        Debug.Log("bufferstuff: " + RustInterop.AddXToEachElement(arr, 6));
+        //Debug.Log("Add X to each Element: " + RustInterop.AddXToEachElement(2, arr, 3));
+        //byte[] jeu = RustInterop.AddXToEachElement(2, arr, 3);
+    }
+}
